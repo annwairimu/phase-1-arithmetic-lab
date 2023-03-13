456b2ba6ef4b5b116432f108c479e99180deae95
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-03-13 21:53:28 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
 
-// Write your code here
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+let numbers = [5, 10, 20, 15, 7];
+let max = Math.max(...numbers);
